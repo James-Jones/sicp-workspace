d96af3def648dce3d7949b2ecd1a439208b1ab9e
James Jones <james.jones.jmj@gmail.com>
2012-04-28 10:29:34 +0100

    Added another solution to exercise 1.3

--- ex1dot3.scm
+++ ex1dot3.scm
@@ -1,7 +1,15 @@
 ; Exercise 1.3: Define a procedure that takes three numbers as arguments
 ; and returns the sum of the squares of the two larger numbers.
 
-(define (sum-of-greater-squares a b c)
+; Here are two solutions
+
+(define (first-sum-of-greater-squares a b c)
+	(+ 
+		(* (max a b c) (max a b c)) ; Sqaure the larget term
+		; Now square the second largest term.
+		(* (min (max a b) (max b c) (max a c)) (min (max a b) (max b c) (max a c)))))
+
+(define (second-sum-of-greater-squares a b c)
 	(+ 
 		(* (max a b c) (max a b c)) ; Sqaure the larget term
 		; Now square the second largest term. Uses conditionals to find the second largest term.
@@ -25,14 +33,24 @@
 				((= b c) b)
 	            (else c)))))
 
-; Some test for correctness
+; Some tests for correctness
 
 (newline)
-(write (sum-of-greater-squares 6 4 6)) ; Two are the same. Expect 6*6 + 6*6 = 72
+(write (first-sum-of-greater-squares 6 4 6)) ; Two are the same. Expect 6*6 + 6*6 = 72
 (newline)
-(write (sum-of-greater-squares 3 4 5)) ; Increasing. Expect 4*4 + 5*5 = 41
+(write (first-sum-of-greater-squares 3 4 5)) ; Increasing. Expect 4*4 + 5*5 = 41
 (newline)
-(write (sum-of-greater-squares 5 4 3)) ; Decreasing. Expect 5*5 + 4*4 = 41
+(write (first-sum-of-greater-squares 5 4 3)) ; Decreasing. Expect 5*5 + 4*4 = 41
 (newline)
-(write (sum-of-greater-squares 3 4 2)) ; Middle is the largest. Expect 3*3 + 4*4 = 25
+(write (first-sum-of-greater-squares 3 4 2)) ; Middle is the largest. Expect 3*3 + 4*4 = 25
+(newline)
+
+(newline)
+(write (second-sum-of-greater-squares 6 4 6)) ; Two are the same. Expect 6*6 + 6*6 = 72
+(newline)
+(write (second-sum-of-greater-squares 3 4 5)) ; Increasing. Expect 4*4 + 5*5 = 41
+(newline)
+(write (second-sum-of-greater-squares 5 4 3)) ; Decreasing. Expect 5*5 + 4*4 = 41
+(newline)
+(write (second-sum-of-greater-squares 3 4 2)) ; Middle is the largest. Expect 3*3 + 4*4 = 25
 (newline)
